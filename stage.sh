196b2a332af66b9e4e2e1a8aa0cc5a6e1e827249
Mark Greenwald <Mark@Greenwald>
2025-03-26 13:11:44 -0700

    switch /web -> /docs

--- stage.sh
+++ stage.sh
@@ -1,7 +1,4 @@
-rm -rf ./web
-mkdir ./web
-node package.js
 sh document.sh
-cp -r ./docs/* ./web
-cp -r ./packages/* ./web
-cp -r ./binders/* ./web
+node package.js
+cp -r ./packages/* ./docs
+cp -r ./binders/* ./docs
